89993825bc0bc9a2c0c5f2bdf68a19d3dfe72794
Дмитрий Неганов <neganov.dima@gmail.com>
2025-02-16 03:03:52 +0300

    fix(NumberOfFiles): skip files listed in .gitignore

--- src/metric_classes/number_of_files.py
+++ src/metric_classes/number_of_files.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from typing import List
 from clang.cindex import Config, Index
 
@@ -34,11 +35,16 @@ class NumberOfFiles:
             Config.set_library_file("/path/to/libclang.dll")
         index = Index.create()
 
+        # Gets all tracked (--cached) and untracked (--others) filenames except standard git
+        # exclusions: .gitignore in each directory, and the user’s global exclusion file.
+        filenames = subprocess.run(
+            args=["git", "ls-files", "--cached", "--others", "--exclude-standard"],
+            cwd=repo_path, capture_output=True, text=True).stdout.splitlines()
+
         file_cursors = []
-        for root, _, files in os.walk(repo_path):
-            for file in files:
-                _, ext = os.path.splitext(file)
-                if ext in self.__extensions:
-                    cursor = index.parse(os.path.join(root, file), args=['-std=c++17']).cursor
-                    file_cursors.append(FileCursor(cursor, os.path.join(root, file)))
+        for file in filenames:
+            _, ext = os.path.splitext(file)
+            if ext in self.__extensions:
+                cursor = index.parse(os.path.join(repo_path, file)).cursor
+                file_cursors.append(FileCursor(cursor, os.path.join(repo_path, file)))
         return file_cursors
